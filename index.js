c10888162f3b237edc6f9c273900831e2a7c465f
Alex Johnson <alex@johnsonwebware.com>
2016-06-18 14:37:20 -0700

    Started template

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-global.root   = __dirname;
-global.lib    = global.root + '/lib';
+global.appRoot   = __dirname;
+global.lib    = global.appRoot + '/lib';
 global.config = require('./config')[process.env.NODE_ENV || 'development'];
 global.util   = require('util');
 
 require(global.lib + '/init')
 	.set('view engine', 'pug')
-	.locals.pretty = process.env.NODE_ENV == 'production';
+	.locals.pretty = !(process.env.NODE_ENV == 'production');
